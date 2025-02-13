28c420c7a10da854bd916102950fb65736773fd6
XuzhengChen <87419015+XuzhengChen@users.noreply.github.com>
2025-02-13 23:41:59 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,6 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'XuzhengChen', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '海底捞爱好者', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://chenxuzheng.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
